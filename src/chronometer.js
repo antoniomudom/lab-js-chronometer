6f6c613c1bdc84eb78bbb6ce12c91f584911dbcc
Antonio Muñoz <antoniomunozdominguez@gmail.com>
2023-07-18 19:13:01 +0200

    Hecho con trabajito

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,29 +1,45 @@
 class Chronometer {
   constructor() {
+    this.currentTime=0;
+    this.intervalId=null;
     // ... your code goes here
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+
+     this.intervalId=setInterval(() => {
+       this.currentTime++;
+       if(printTimeCallback){
+        printTimeCallback();
+       }
+    }, 1000)
+    // Este lo he sacado gracias a las preguntas del final de las instrucciones
+    //No obstante no me queda del todo claro el condicional
   }
 
   getMinutes() {
+    return Math.floor(this.currentTime/60);
     // ... your code goes here
   }
 
   getSeconds() {
+    return this.currentTime % 60;
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
-  }
+    return value.toString().padStart(2,"0")   // ... your code goes here
+  }//Este lo hemos buscado la verdad porque no sabíamos con .Slice
+  //Lo bueno es que hemos aprendido ya que entendemos el método toString y padStart
+
 
   stop() {
+    clearInterval(this.intervalId)
     // ... your code goes here
   }
 
   reset() {
+    this.currentTime=0;
     // ... your code goes here
   }
 
